5e321a5e8e69045080e021ca606b02dcb740fd52
Rahman <rahmansaif.413@gmail.com>
2024-02-23 16:13:30 +0600

    added a utility class that contains the functions

--- script.js
+++ script.js
@@ -3,25 +3,9 @@ let plus=document.querySelector(".increment");
 let minus=document.querySelector(".decrement");
 let reset=document.querySelector(".reset");
 let result=document.querySelector(".result");
-let count=0;
 
-plus.addEventListener("click",function(){
-    count++;
-    result.textContent=count;
-    
-})
+//changing value of count variables with button clicks
+plus.addEventListener("click",increaseCount);
+minus.addEventListener("click",decreaseCount);
+reset.addEventListener("click",resetCount);
 
-minus.addEventListener("click",function(){
-    if(count==0){
-        count=0;
-    }else{
-        count--;
-    }
-    
-    result.innerText=count;
-})
-
-reset.addEventListener("click",function(){
-    count=0;
-    result.innerText=count;
-})
